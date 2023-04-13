d2aac08eef86e78e91ed7fc0c359bb207594227b
MLibralato91 <michelelibralato911@gmail.com>
2023-04-13 15:20:06 +0200

    add daja.js

---
+++ src/data/data.js
@@ -0,0 +1,220 @@
+const links = [
+  {
+    text: "Characters",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Comics",
+    url: "#",
+    current: true,
+  },
+  {
+    text: "Movies",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Tv",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Games",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Collectibles",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Videos",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Fans",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "News",
+    url: "#",
+    current: false,
+  },
+  {
+    text: "Shop",
+    url: "#",
+    current: false,
+  },
+];
+const products = [
+  {
+    src: "../assets/images/buy-comics-digital-comics.png",
+    titolo: "Digital Comics",
+  },
+  {
+    src: "../assets/images/buy-comics-merchandise.png",
+    titolo: "Dc Merchandise",
+  },
+  {
+    src: "../assets/images/buy-comics-subscriptions",
+    titolo: "Subscription",
+  },
+  {
+    src: "../assets/images/buy-comics-shop-locator.png",
+    titolo: "Comic shop locator",
+  },
+  {
+    src: "../assets/images/buy-dc-power-visa.svg",
+    titolo: "Dc power visa",
+  }
+];
+const menu = [
+  {
+    title: 'dc comics',
+    links: [
+      {
+        text: "Characters",
+        url: "#",
+      },
+      {
+        text: "Comics",
+        url: "#",
+      },
+      {
+        text: "Movies",
+        url: "#",
+      },
+      {
+        text: "TV",
+        url: "#",
+      },
+      {
+        text: "Games",
+        url: "#",
+      },
+      {
+        text: "Videos",
+        url: "#",
+      },
+      {
+        text: "News",
+        url: "#",
+      },
+    ]
+  },
+  {
+    title: 'dc',
+    links: [
+      {
+        text: "Terms Of Use",
+        url: "#",
+      },
+      {
+        text: "Provacy policy(New)",
+        url: "#",
+      },
+      {
+        text: "Ad Choices",
+        url: "#",
+      },
+      {
+        text: "Advertising",
+        url: "#",
+      },
+      {
+        text: "Jobs",
+        url: "#",
+      },
+      {
+        text: "Subscriptions",
+        url: "#",
+      },
+      {
+        text: "Talent Workshops",
+        url: "#",
+      },
+      {
+        text: "CPSC Certificates",
+        url: "#",
+      },
+      {
+        text: "Ratings",
+        url: "#",
+      },
+      {
+        text: "Shop Help",
+        url: "#",
+      },
+      {
+        text: "Contact Us",
+        url: "#",
+      },
+    ]
+  },
+  {
+    title: 'sites',
+    links: [
+      {
+        text: "DC",
+        url: "#",
+      },
+      {
+        text: "MAD Magazine",
+        url: "#",
+      },
+      {
+        text: "DC Kids",
+        url: "#",
+      },
+      {
+        text: "DC Universe",
+        url: "#",
+      },
+      {
+        text: "DC Power Visa",
+        url: "#",
+      }
+    ]
+  },
+  {
+    title: 'shop',
+    links: [
+      {
+        text: "Shop DC",
+        url: "#",
+      },
+      {
+        text: "Shop DC Collectibles",
+        url: "#",
+      }
+
+    ]
+  },
+
+];
+const contacts = [
+  {
+    src: "../assets/images/footer-facebook.png",
+    titolo: "Facebook",
+  },
+  {
+    src: "../assets/images/footer-twitter.png",
+    titolo: "Twitter",
+  },
+  {
+    src: "../assets/images/footer-youtube.png",
+    titolo: "Youtube",
+  },
+  {
+    src: "../assets/images/footer-pinterest.png",
+    titolo: "Pinterest",
+  },
+  {
+    src: "../assets/images/footer-periscope.png",
+    titolo: "Periscope",
+  },
+]
