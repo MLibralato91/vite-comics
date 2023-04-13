411e60f30aa1497beed682e9e59a9eb7bd6d9b24
MLibralato91 <michelelibralato911@gmail.com>
2023-04-13 14:00:55 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1>Ciao</h1>
+    <HeaderComponent/>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+  export default {
+    name: 'App',
+    components: {
+      HeaderComponent
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
